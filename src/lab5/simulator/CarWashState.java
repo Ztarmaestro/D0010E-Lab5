79e9ef45b5db8fa6abd3350c4ccd4ee88787d55b
weedz <weedzcokie@gmail.com>
2014-03-05 10:01:32 +0100

    added variables and methods to CarWashState

--- src/lab5/simulator/CarWashState.java
+++ src/lab5/simulator/CarWashState.java
@@ -2,4 +2,24 @@ package lab5.simulator;
 
 public class CarWashState extends SimState{
 
+	private int rejectedCars;
+	private double idleTime;
+	private double queueTime;
+	
+	public CarWashState() {
+		
+	}
+	
+	public double getIdleTime() {
+		return idleTime;
+	}
+	
+	public double getQueueTime() {
+		return queueTime;
+	}
+	
+	public int getRejectedCars() {
+		return rejectedCars;
+	}
+	
 }
